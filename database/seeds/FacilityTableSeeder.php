9edada52b4209c48d6be935681fe7a1685509557
Patrick D Koe <pdaniel.koe@gmail.com>
2021-07-06 19:24:32 +0800

    fix typo caused FacilityTableSeeder unable to run
    Conflict between naming Facility and Facilities #14

--- database/seeds/FacilityTableSeeder.php
+++ database/seeds/FacilityTableSeeder.php
@@ -13,6 +13,6 @@ class FacilityTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(Facilty::class, 25)->create();
+        factory(Facility::class, 25)->create();
     }
 }
