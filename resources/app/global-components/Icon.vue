3ecdb065781b62761542ca6d0416beecb4518d59
chaitak-gorai <chaitakgorai@gmail.com>
2023-04-03 01:58:43 +0530

    Added icon template for svg icons

--- resources/app/global-components/Icon.vue
+++ resources/app/global-components/Icon.vue
@@ -681,13 +681,27 @@
          viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <path d="M13 5l7 7-7 7M5 5l7 7-7 7" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
     </svg>
+    <svg v-else-if="name === 'home-icon'" class="h-5 w-5" stroke="currentColor" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+    >
+    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"/>
+    </svg>
+    <svg v-else-if="name === 'hamburger'" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path v-if="!isMenuOpen" stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+  <path v-if="isMenuOpen" stroke-linecap="round" stroke-linejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+</svg>
+<svg v-else-if="name === 'chat-bubble'" class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" >
+    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="{2}" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
+ </svg>
+
 </template>
 <script>
 export default {
     name: 'Icon',
     props: {
         name: String,
-    }
+        isMenuOpen: Boolean,
+    },
+
 }
 
 </script>
